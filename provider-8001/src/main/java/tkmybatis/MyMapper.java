fc51fd5f359b100871ce42a9a3d766ac6cd3e28b
zhanghongy <865383353@qq.com>
2019-04-24 15:30:43 +0800

    zipkin

---
+++ provider-8001/src/main/java/tkmybatis/MyMapper.java
@@ -0,0 +1,11 @@
+package tkmybatis;
+
+import tk.mybatis.mapper.common.Mapper;
+import tk.mybatis.mapper.common.MySqlMapper;
+
+/**
+ * @Author: 张宏运
+ * @Date: 2019-04-24 15:27
+ */
+public interface MyMapper<T> implements Mapper<T>, MySqlMapper<T> {
+}
